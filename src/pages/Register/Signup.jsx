487fca8f1de536a5aee3027fa4c54e9c9db862de
Nanthesh Kannan <nantheshkannan@Nantheshs-MacBook-Pro.local>
2024-10-09 16:50:43 -0400

    register page

---
+++ src/pages/Register/Signup.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+const Signup = () => {
+    
+        const [showPassword, setShowPassword] = React.useState(false);
+      
+        const handleClickShowPassword = () => setShowPassword((show) => !show);
+      
+        const handleMouseDownPassword = (event) => {
+          event.preventDefault();
+        };
+      
+        const handleMouseUpPassword = (event) => {
+          event.preventDefault();
+        };
+      
+  return (
+    <Box>
+    <Card>
+        <Typography variant="h3">
+        Sign up 
+      </Typography>
+      <TextField
+          required
+          id="outlined-required"
+          label="Email"
+        />
+        <FormControl sx={{ m: 1, width:'25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  onMouseUp={handleMouseUpPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+    </Card>
+    </Box>
+  )
+}
+
+export default Signup
